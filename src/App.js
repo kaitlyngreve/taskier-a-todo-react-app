8899a20e5a1b0efd204844b4887eab0f82b567ce
Kaitlyn Greve <kgreve14@gmail.com>
2022-11-01 14:31:36 -0700

    app built out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+  const savedTodos = localStorage.getItem("todos");
+
+  const [todos, setTodos] = useState(savedTodos ? JSON.parse(savedTodos) : []);
+  const [todo, setTodo] = useState("");
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState('');
+
+  console.log('hello');
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false
+    }
+    // add our newTodo to our todos
+    setTodos([...todos, newTodo])
+    // set the input to blank 
+    setTodo("")
+  }
+
+  function deleteTodo(id) {
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+
+    setTodos(updatedTodos)
+  }
+
+  function toggleComplete(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  function editTodo(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+    setTodoEditing(null)
+    setEditingText(todo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-app'>
+      <h1>T a s k i e r</h1>
+      <div className='todo-container'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button className='button' type="submit">add task</button>
+        </form>
+        {todos.map((todo) => <div className='todo-item' key={todo.id}>
+          <input
+            type="checkbox"
+            onChange={() => toggleComplete(todo.id)}
+            checked={todo.completed}
+          />
+
+          {todoEditing === todo.id ? (<input
+            type="text"
+            value={editingText}
+            onChange={(e) => setEditingText(e.target.value)}
+          />)
+            :
+            (<div className='todo-item-text'>{todo.text}</div>)}
+
+
+          <button className='button button-deleteTodo' onClick={() => deleteTodo(todo.id)}>x</button>
+
+          {todoEditing === todo.id ?
+            (<button className='button button-submitEdits' onClick={() => editTodo(todo.id)}>Submit</button>)
+            :
+            (<button className='button button-editTodo' onClick={() => setTodoEditing(todo.id)}>Edit Todo</button>)}
+
+        </div>)}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
